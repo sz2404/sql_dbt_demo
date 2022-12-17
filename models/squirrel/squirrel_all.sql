3445ac624348c6a098459f23d883352b9d9a1c03
SZ2404@nyu.edu <SZ2404@nyu.edu>
2022-12-17 06:04:02 +0000

    folder

---
+++ models/squirrel/squirrel_all.sql
@@ -0,0 +1,2 @@
+select *
+from `sql-dbt-demo.SQL_DBT_demo.Squirrel_2018_census`
--- models/squirrel_all.sql
+++
@@ -1,2 +0,0 @@
-select Unique_Squirrel_ID, Primary_Fur_Color, Highlight_Fur_Color, Combination_of_Primary_and_Highlight_Color from
-`sql-dbt-demo.SQL_DBT_demo.Squirrel_2018_census`
