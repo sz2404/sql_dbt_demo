83e7291d8d2b55a9265c2a2b4a27b93cdf29088b
SZ2404@nyu.edu <SZ2404@nyu.edu>
2022-12-19 21:47:25 +0000

    Clean partners

--- models/gg_project/staging/partners.sql
+++ models/gg_project/staging/partners.sql
@@ -1,3 +1,16 @@
+WITH CTE AS (
+    SELECT
+        id, 
+        CAST(created_at/1000000000 AS float64) AS nc, 
+        CAST(updated_at/1000000000 AS float64) AS nu,
+        partner_type,
+        lead_sales_contact
+    FROM {{ source('SQL_DBT_demo', 'partner')}})
+
 SELECT
-*
-FROM {{ source('SQL_DBT_demo', 'partner')}}
+    id, 
+    DATE(TIMESTAMP_SECONDS(cast(nc as int64))) as created_at,
+    DATE(TIMESTAMP_SECONDS(cast(nu as int64))) as updated_at,
+    partner_type,
+    lead_sales_contact
+FROM CTE
